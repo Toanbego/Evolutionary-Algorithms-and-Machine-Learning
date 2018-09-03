a00467329bd4448c3d0b72e393a2156f5f8c926c
Torstein <torstein.gombos@gmail.com>
2018-09-03 17:32:20 +0200

    initialized population

---
+++ Exercises/Exercise 2/mutations.py
@@ -0,0 +1,8 @@
+"""
+Python file for testing crossovers and recombinations
+"""
+import numpy as np
+
+#Initialize population
+node_1 = np.array([2, 4, 7, 1, 3, 6, 8, 9, 5])
+node_2 = np.array([5, 9, 8, 6, 2, 4, 1, 3, 7])
